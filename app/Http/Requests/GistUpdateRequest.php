83e823ee8bf27878b577f89bb7471bf92e61b560
azeezafeez <chillingloccin1@gmail.com>
2021-04-20 08:28:19 +0100

    deleting gists

--- app/Http/Requests/GistUpdateRequest.php
+++ app/Http/Requests/GistUpdateRequest.php
@@ -49,14 +49,5 @@ class GistUpdateRequest extends FormRequest
     }
 
 
-<<<<<<< HEAD
-=======
-    protected function failedAuthorization()
-    {
-
-
-    }
-
->>>>>>> 052e7eb315ae2ca1639806c7b584010d953f0844
 
 }
